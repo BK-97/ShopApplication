78dad59cb958104374bbbfc42042aad429e26519
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2023-11-17 17:37:02 +0300

    update

--- src/MenuScreen.js
+++ src/MenuScreen.js
@@ -4,13 +4,15 @@ import { View } from 'react-native';
 import VerticalList from './VerticalList';
 import { styles } from './styles';
 
-const MenuScreen = () => {
+const MenuScreen = ({ navigation }) => {
   const handleCategoryPress = (category) => {
     console.log('Seçilen Kategori:', category);
+    // Alt kategori sayfasına yönlendirme işlemleri burada yapılacak
+    navigation.navigate('Products', { selectedCategory: category });
   };
+
   return (
     <View style={styles.verticalContainer}>
-      
       <VerticalList handleCategoryPress={handleCategoryPress} />
     </View>
   );
